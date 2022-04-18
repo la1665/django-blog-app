64f9724078dc40346ba25947937af87b1f40ab84
amir <amirsazegar16@gmail.com>
2022-04-18 14:57:48 +0330

    full app

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from email.policy import default
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+    def __str__(self) -> str:
+        return self.title
